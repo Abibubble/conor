eb4c00af55f171c363054561af31970ccb3dc7d6
Abibubble <abi.yamaha.neo@gmail.com>
2021-04-01 17:20:33 +0000

    feat: Add hover function on socials links

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,29 @@ function copyrightYear() {
 }
 
 copyrightYear();
+
+// -------------------------------------------------------------------- Hover functions
+
+// Change the colors of socials links
+function hover(id){
+    let hoverLink = document.getElementById(id);
+    switch (id) {
+        case "facebook":
+            hoverLink.style.color = "#1298f6";
+            break;
+        case "instagram":
+            hoverLink.style.color = "#c9006b";
+            break;
+        case "youtube":
+            hoverLink.style.color = "#ff0000";
+            break;
+        default:
+            hoverLink.style.color = "#fff";
+            break;
+    }    
+}
+
+// Reset to normal after hover
+function normal(id) {
+	document.getElementById(id).style.color = "#fff";
+}
