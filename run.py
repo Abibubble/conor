9e22e01112bfc60215f44a45f67aa8f64f1820dd
Abibubble <abi.yamaha.neo@gmail.com>
2021-04-07 16:24:59 +0000

    wip: Get form working

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
-    return render_template("index.html", page_title="Contact")
+        flash(
+            "Thanks {}, we have received your message!".format(
+                request.form.get("name")))
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
