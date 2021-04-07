c45782e8ddcc3825bd3f42bb0296c5891447d170
Abibubble <abi.yamaha.neo@gmail.com>
2021-04-07 16:02:47 +0000

    wip: Add Flask and forms

---
+++ run.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
+    return render_template("index.html", page_title="Contact")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
